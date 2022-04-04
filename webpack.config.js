55ba669a066f68d04c1dc5afdf856d23464343e3
jzamora5 <jzamora_5@yahoo.com>
2022-04-04 09:01:34 -0500

    Bundle analyzer and reduce bundle size

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,19 @@
 const webpack = require('webpack')
 const path = require('path')
+const shouldAnalyze = process.argv.includes('--analyze')
+
+// npm run build -- --analyze
 
 const nodeEnv = process.env.NODE_ENV || 'development'
+
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin
+
+const plugins = []
+
+if (shouldAnalyze) {
+  plugins.push(new BundleAnalyzerPlugin())
+}
 
 const config = {
   mode: nodeEnv,
@@ -14,6 +26,7 @@ const config = {
   devServer: {
     contentBase: '.',
   },
+  plugins,
 }
 
 module.exports = config
