fb0caab8ad8ec860f2f3368f2650b41977dda2f1
jzamora5 <jzamora_5@yahoo.com>
2022-04-08 08:38:16 -0500

    Lazy Loading

--- src/modal/index.js
+++ src/modal/index.js
@@ -1,11 +1,17 @@
-import { openModal } from './open-modal'
+// import { openModal } from './open-modal'
 
 export const modalListener = event => {
   event.preventDefault()
   const img = event.target
   const link = img.parentElement
+  const videoId = link.dataset.videoid
 
   if (link && link.classList.contains('js-video-link')) {
-    openModal(link.dataset.videoid)
+    // Lazy Loading
+    import(/* webpackChunkName: "modal" */ './open-modal').then(
+      ({ openModal }) => {
+        openModal(videoId)
+      }
+    )
   }
 }
